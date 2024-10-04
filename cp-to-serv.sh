979a9228234de1b67a821f451ab029000a742c30
Kenneth Bruce <me@kenneth-bruce.com>
2024-10-04 15:09:27 +0000

    super dumb script

---
+++ cp-to-serv.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ex
+
+sudo cp ./index.html /var/www/html/links/index.html
+sudo cp ./index.js /var/www/html/links/index.js
+sudo cp ./index.css /var/www/html/links/index.css
+sudo cp ./reset.css /var/www/html/links/reset.css
